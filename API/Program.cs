ee5b28e02285c34387dd5cae98cb528be8b74fb8
Vasyl Tkachenko <vasyl.tkachenko@nixs.com>
2024-05-23 16:52:23 +0200

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -30,7 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors(opt =>
 {
-    opt.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 });
 
 //app.UseHttpsRedirection();
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -7,6 +7,8 @@ import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
 
 export const router = createBrowserRouter([
     {
@@ -20,7 +22,9 @@ export const router = createBrowserRouter([
             { path: 'contact', element: <ContactPage /> },
             { path: 'server-error', element: <ServerError /> },
             { path: 'not-found', element: <NotFound /> },
-            { path: '*', element: <Navigate replace to='/not-found' />}
+            { path: 'basket', element: <BasketPage /> },
+            { path: 'checkout', element: <CheckoutPage /> },
+            { path: '*', element: <Navigate replace to='/not-found' /> }
 
 
         ]
