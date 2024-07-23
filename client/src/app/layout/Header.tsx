0b57d3ff4fd5680958853b722b8aadb113b8c04d
Vasyl Tkachenko <vasyl.tkachenko@nixs.com>
2024-07-23 21:17:41 +0200

    End of section 9

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -3,6 +3,7 @@ import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typogr
 import { Link, NavLink } from "react-router-dom";
 //import { useStoreContext } from "../context/StoreContext";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 const midLinks = [
     { title: 'catalog', path: '/catalog' },
@@ -33,7 +34,9 @@ const navStyles = {
 }
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
-    const {basket} = useAppSelector(state => state.basket);
+    const { basket } = useAppSelector(state => state.basket);
+    const { user } = useAppSelector(state => state.account);
+
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
 
     return (
@@ -70,18 +73,22 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                         </Badge>
                     </IconButton>
 
-                    <List sx={{ display: 'flex' }}>
-                        {rightLinks.map(({ title, path }) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
+                    {user ? (
+                        <SignedInMenu />
+                    ) : (
+                        <List sx={{ display: 'flex' }}>
+                            {rightLinks.map(({ title, path }) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={navStyles}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                        </List>
+                    )}
                 </Box>
 
             </Toolbar>
